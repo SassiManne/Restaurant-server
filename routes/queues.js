e63e2cee3fa3c7613c637b6c420679bb9a898f31
SassiManne <sasimane7@gmail.com>
2022-06-28 11:37:10 +0300

    complite sit by priority

--- routes/queues.js
+++ routes/queues.js
@@ -6,32 +6,40 @@ const router = express.Router();
 
 router.use(express.json());
 
-const getData = () => {
+
+const getQueueData = () => {
 
     let queues = fs.readFileSync('./dataBase/queues.json');
     let allQueues = JSON.parse(queues)
 
+    return allQueues
+}
+
+const getTableData = () => {
+
+
     let tables = fs.readFileSync('./dataBase/tables.json');
     let allTables = JSON.parse(tables)
 
-    return [allQueues, allTables]
+    return allTables
 }
 
-const saveData = ( foundQueue, foundTable) => {
-    let queues = fs.writeFileSync('./dataBase/queues.json')
-    let updatedQueue = JSON.stringify(foundQueue)
+// const saveData = ( foundQueue, foundTable) => {
+//     let queues = fs.writeFileSync('./dataBase/queues.json')
+//     let updatedQueue = JSON.stringify(foundQueue)
 
-    let tables = fs.writeFileSync('./dataBase/tables.json')
-    let updatedTable = JSON.stringify(foundTable)
+//     let tables = fs.writeFileSync('./dataBase/tables.json')
+//     let updatedTable = JSON.stringify(foundTable)
 
 
-}
+// }
 
 
 //////////get time and date ///////////
 const dateTime = () => {
 
     var timeStamp = new Date().toLocaleString()
+
 
     return timeStamp
 }
@@ -41,7 +49,8 @@ const dateTime = () => {
 
 router.get('/sitByPriority', (req, res) => {
 
-    let [queueData, tableData] = getData();
+    let queueData = getQueueData();
+    let tableData = getTableData();
 
     let foundQueue = queueData.filter((group) => group.queue === "tobesitted");
     let foundTable = tableData.filter((table) => table.GroupSeqNum === null);
@@ -62,34 +71,36 @@ router.get('/sitByPriority', (req, res) => {
 
 
     /////reverse sort to the size of groups
-    function compareQueue(a, b) {
-        if (+a.size > +b.size) {
-            return -1;
-        }
-        if (+a.size < +b.size) {
-            return 1;
-        }
-        return 0;
+    
+    function getMaxGroup (a ,b) {
+        return ((+b.size) - (+a.size));
+    }
+
+    /////sort time stamp
+    function sortByTime (a ,b) {
+        return ((+a.timeStamp) - (+b.timeStamp));
     }
 
     ////// sort for capacity of the tables
     function compareTable(a, b) {
-        if (+a.capacity < +b.capacity) {
-            return -1;
-        }
-        if (+a.capacity > +b.capacity) {
-            return 1;
-        }
-        return 0;
+        return ((+a.capacity) - (+b.capacity));
     }
 
-    foundQueue.sort(compareQueue);
+
+
+    let sortMax =  foundQueue.sort(getMaxGroup);
+    let sortTime = sortMax.sort(sortByTime);
+    console.log(sortTime)
+
     foundTable.sort(compareTable);
+    
+
+
     
 
     let matchTable, matchQueue, matchBreak;
     
-    for (let i of foundQueue) {
+    for (let i of sortTime) {
         for (let j of foundTable) {
 
             if (foundTable.at(-1).capacity < i.size){break}
@@ -136,8 +147,8 @@ router.get('/sitByPriority', (req, res) => {
 //GET all queues
 router.get('/all', (req, res) => {
 
-    let allQueues = getData();
-    res.send(allQueues[0]);
+    let allQueues = getQueueData();
+    res.send(allQueues);
 });
 
 
@@ -145,7 +156,7 @@ router.get('/all', (req, res) => {
 router.get('/queu/:queue', (req, res) => {
 
     const { queue } = req.params;
-    let groupByStatus = getData();
+    let groupByStatus = getQueueData();
 
     const foundGroup = groupByStatus[0].filter((group) => group.queue === queue);
 
@@ -156,7 +167,7 @@ router.get('/queu/:queue', (req, res) => {
 //GET group by "GroupSeqNo"
 router.get('/:GroupSeqNo', (req, res) => {
     const { GroupSeqNo } = req.params;
-    let ourQueues = getData();
+    let ourQueues = getQueueData();
 
     const foundGroup = ourQueues[0].find((group) => group.GroupSeqNo === GroupSeqNo);
 
@@ -166,11 +177,11 @@ router.get('/:GroupSeqNo', (req, res) => {
 
 
 
-
 //POST request
 router.post('/', (req, res) => {
     let createGroup = req.body;
-    let pars = getData();
+    let pars = getQueueData();
+    console.log(pars)
     req.body.queue = "tobesitted";
     req.body.table = "null";
     req.body.timeStamp = dateTime();
@@ -186,7 +197,7 @@ router.post('/', (req, res) => {
 //DELETE group by "GroupSeqNo"
 router.delete('/:GroupSeqNo', (req, res) => {
     const GroupSeqNo = req.params.GroupSeqNo;
-    let allGroups = getData();
+    let allGroups = getQueueData();
     let tempGroups = allGroups.filter((group) => group.GroupSeqNo !== GroupSeqNo);
 
     fs.writeFileSync('./dataBase/queues.json', JSON.stringify(tempGroups));
@@ -198,7 +209,7 @@ router.delete('/:GroupSeqNo', (req, res) => {
 //UPDATE queue by "GroupSeqNo"
 router.patch('/:GroupSeqNo', (req, res) => {
     const { GroupSeqNo } = req.params;
-    let allGroups = getData();
+    let allGroups = getQueueData();
 
     let tempGroups = allGroups.filter((group) => group.GroupSeqNo !== GroupSeqNo);
     req.body.GroupSeqNo = GroupSeqNo
